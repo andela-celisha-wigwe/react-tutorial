096f731853f2de94d730afa5d24b44d9fd219404
ELISHA-WIGWE CHIJIOKE O <elchroy@ELISHA-WIGWEs-MacBook-Pro.local>
2016-07-18 13:11:29 +0100

    Start react with facebook

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -1,10 +1,15 @@
+var data = [
+	{id:1, author:"Pete HUnt", text:"This is one comment"},
+	{id:2, author:"Jordan Walke", text:"This is *another* comment"}
+];
+
 var CommentBox = React.createClass({
     render: function () {
       return (
       		<div className="commentBox">
       			Hello Word, i am a comment box. do you like me?
       			<h1> Comments</h1>
-      			<CommentList />
+      			<CommentList data={this.props.data} />
       			<CommentForm />
       		</div>
       	);
@@ -14,16 +19,23 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
 	render: function () {
+		var commentNodes = this.props.data.map(function (comment) {
+			return (
+				<Comment author={comment.author} key={comment.id}>
+					{comment.text}
+				</Comment>
+			);
+		});
 		return (
 			<div className="commentList">
-				Hello, world, I am just a commentlist.
+				{commentNodes}
 			</div>
-		)
+		);
 	}
 });
 
 
-var CommentForm == React.createClass({
+var CommentForm = React.createClass({
 	render: function () {
 		return (
 			<div className="commentForm">
@@ -35,19 +47,28 @@ var CommentForm == React.createClass({
 
 
 var Comment = React.createClass({
+
+	rawMarkup: function () {
+		var md = new Remarkable();
+		var rawMarkup = md.render(this.props.children.toString());
+		return { __html: rawMarkup };
+	},
+
 	render: function () {
+		var md = new Remarkable();
 		return (
 			<div className="comment">
 				<h2 className="commentAuthor">
 				{this.props.author}
-				<h2>
-				{this.props.children}
+				</h2>
+				<span dangerouslySetInnerHTML={this.rawMarkup()} />
+				{md.render(this.props.children.toString())}
 			</div>
 		);
 	}
 });
 
 ReactDOM.render(
-	<CommentBox />,
+	<CommentBox url="/api/comments" />,
 	document.getElementById('content')
 );
